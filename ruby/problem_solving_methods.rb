9bcd137027fdfb6f069923e74afb353872b71839
Kristen Shaw <kristenseipel@gmail.com>
2017-06-08 15:44:39 -0700

    Add release 0

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,9 +1,14 @@
-# def search(array)
-# 	array.each do |x|
-# 		if 
 
-# # arr = [2, 4, 6, 8, 10, 12]
-#  while i < arr.length
+def find_it(array_num)
+my_arr = [15, 8, 3, 2, 9]
+i = 0
+    while i < my_arr.length
+    puts i
+    i+= 1
+	end
+end
+puts find_it(8)
+
 
 def fib(x)
 	n= x-2
@@ -11,3 +16,30 @@ def fib(x)
 
 end
 p fib(100)
+ 
+# release 2
+array = [1, 2, 4, 5, 8, 7, 3, 6]
+def bubble_sort(array)
+  n = array.length
+  # this line determines how many numbers are to be sorted in this paraticular array
+  loop do
+    swapped = false
+    # The method will continue swapping till the parameter is considered false.
+
+    (n-1).times do |i|
+    	# this portion determines how many times to perform the loop
+      if array[i] > array[i+1]
+      	# this checks to see if the numbers are in oder based on value
+        array[i], array[i+1] = array[i+1], array[i]
+        # this reorders the lesser value number first and places the greater one after. and continues till false
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+p bubble_sort(array)
