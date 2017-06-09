baaa4551f4c42cfa795706fce070c76175bf3113
Kristen Shaw <kristenseipel@gmail.com>
2017-06-09 16:48:48 -0700

    Add prob 2 of array

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -1,15 +1,77 @@
-# Array Drills
+
 
 zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
                               "shotgun", "compass", "CB radio", "batteries"]
+listed_supplies = []
 
-# 1. Iterate through the zombie_apocalypse_supplies array,
-# printing each item in the array separated by an asterisk
-# ----
+zombie_apocalypse_supplies.each do |supply|
+
+	listed_supplies << supply.join("8")
+end
+puts listed_supplies
 
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
+# def compare_letter(a, b)
+# 	if a < b
+# 		-1
+# 	elsif a > b
+# 		1
+# 	else
+# 		0
+# 	end
+# end
+
+# # Compares the given words as one would alphabetize them. As is expected in the English language, letter come before numbers, capital letters are considered equivalent to their lowercase counterparts, and the shorter of two otherwise-equivalent words is considered to come first.
+# # returns -1 if word_a is before word_b, 1 if word_a is after word_b, and 0 if the two words are the same
+# def compare_word(word_a, word_b)
+# 	downcased_word_a = word_a.downcase
+# 	downcased_word_b = word_b.downcase
+# 	for i in 0..(downcased_word_a.length - 1)
+# 		# If word a is shorter than word b
+# 		if i >= (downcased_word_b.length - 1)
+# 			return 1
+# 		end
+
+# 		result = compare_letter(downcased_word_a[i], downcased_word_b[i])
+# 		if result < 0
+# 			return -1
+# 		elsif result > 0
+# 			return 1
+# 		end
+# 	end
+
+# 	# If word a is shorter than word b
+# 	if downcased_word_a.length < downcased_word_b.length
+# 		return -1
+# 	end
+
+# 	return 0
+# end
+
+zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "CB radio", "batteries"]
+# zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars", "shotgun", "compass", "shotgun shells", "CB radio", "batteries"]
+
+sorted_zombie_supplies = []
+zombie_apocalypse_supplies.each do |supply|
+	if sorted_zombie_supplies.empty?
+		sorted_zombie_supplies << supply
+	else
+		for i in 0..(sorted_zombie_supplies.length - 1)
+			# We knew we could use the spaceship operator, but we built our own comparison method for practice
+			# result = compare_word(supply, sorted_zombie_supplies[i])
+			result = supply <=> sorted_zombie_supplies[i]
+			if result <= 0
+				sorted_zombie_supplies.insert(i, supply)
+				break
+			elsif result > 0 && i == (sorted_zombie_supplies.length - 1)
+					sorted_zombie_supplies.push(supply)
+			end
+		end	
+	end
+end
+p sorted_zombie_supplies
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
