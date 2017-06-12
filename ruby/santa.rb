d1f4bd258c8942afdcef2d265b1b1ac849c64843
Kristen Shaw <kristenseipel@gmail.com>
2017-06-12 12:50:22 -0700

    Add initialize method with new attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,7 +7,19 @@ class Santa
 		puts "That was a good #{cookie_type}"
 	end
 
-	def initialize(message)
-		puts "Initializing Santa instance..."
+	def initialize(gender, ethnicity, age)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 	end
 end
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
