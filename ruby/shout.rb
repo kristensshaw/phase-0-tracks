d610c348754694bc88fa9a221de345ea8a4e0d90
Kristen Shaw <kristenseipel@gmail.com>
2017-06-14 10:46:20 -0700

    Add mixin and called module methods on each instance

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,39 +1,44 @@
 
-module Shout
-
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
-
-	def self.yelling_happily(words)
-		words + "!!!" + " :)"
-	end
-end
-
-puts Shout.yell_angrily("arg")
-
-# class Angry_Person
-#   include Shout
-# end
-
-# kristen = Angry_Person.new
-
-# kristen.yell_angrily("WTF, mate")
-# Angry_Person.yell_angrily('WTF')
-
-
 # module Shout
 
-# 	def yell_angrily(words)
+# 	def self.yell_angrily(words)
 # 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :)"
 # 	end
 # end
 
-# class Angry_Person
-#   include Shout
-# end
+# puts Shout.yell_angrily("arg")
 
-# kristen = Angry_Person.new
 
-# kristen.yell_angrily("WTF, mate")
-# Angry_Person.yell_angrily('WTF')
+
+module Shout
+
+	def yell_angrily(words)
+		puts words + "!!!" + " :("
+	end
+
+	def yelling_happily(words)
+		puts words + "!!!" + " :)"
+	end
+end
+
+class Angry_Person
+  include Shout
+end
+
+class Dog
+	include Shout
+end
+
+kristen = Angry_Person.new
+kristen.yell_angrily("WTF, mate")
+kristen.yelling_happily("WTF, mate")
+
+tundra = Dog.new
+tundra.yell_angrily("woof")
+tundra.yelling_happily("woof")
+
+
