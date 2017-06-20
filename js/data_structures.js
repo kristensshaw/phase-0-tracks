87e1bcdbbdc4919fc41d58ced55687d094c729f6
Kristen Shaw <kristenseipel@gmail.com>
2017-06-20 13:14:11 -0700

    Add two instances of car and made adjustments to arguments

--- js/data_structures.js
+++ js/data_structures.js
@@ -23,3 +23,32 @@ for(var i = 0; i < names.length; i++){
 console.log("Horses and their colors");
 console.log(horses);
 
+// Car object
+//===================
+function Car(brand, manufactureYear, isAutomatic){
+
+this.brand = brand;
+
+this.manufactureYear = manufactureYear;
+
+this.isAutomatic = isAutomatic;
+
+this.start = function() {
+  console.log("The car started");
+};
+};
+
+// print data
+var myCar = new Car("BMW", 2011, false);
+console.log("Our car is:");
+console.log(myCar);
+myCar.start();
+myCar.manufactureYear = 2014;
+console.log("After manufacture year adjustment");
+console.log(myCar);
+// Make another car object
+
+var mySecond = new Car("Toyota", 2012, true);
+console.log("Our second car is:");
+console.log(mySecond);
+mySecond.start();
