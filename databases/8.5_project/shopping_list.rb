4d1569e5db11b6911c3f17c024569198f6d74176
Kristen Shaw <kristenseipel@gmail.com>
2017-06-28 11:29:51 -0700

    Separate hear docs for two tables

--- databases/8.5_project/shopping_list.rb
+++ databases/8.5_project/shopping_list.rb
@@ -1,34 +1,43 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new("shopping.db")
-db.results_as_hash = true
+# db.results_as_hash = true
 
-create_table_cmd = <<-SQL
+create_users_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
+    name VARCHAR(255),
     item_id INT, 
     FOREIGN KEY (item_id) REFERENCES items(item_id)
   )
+SQL
 
+create_items_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS items (
     item_id INTEGER PRIMARY KEY, 
     item_name VARCHAR(255),
     item_quantity INT
-    )
+  )
 SQL
 
-db.execute(create_table_cmd)
+db.execute(create_users_table_cmd)
+db.execute(create_items_table_cmd)
+
+
+
+
+
+
 
 def create_user(db, name)
   db.execute("INSERT INTO users (name) VALUES (?)", [name])
 end
 
-def create_list(db, list_hash, items, quantity)
-  db.execute("INSERT INTO items (item_name, item_quantity) VALUES (?, ?)", [item_name, item_quantity])
-  list_hash = {}
-  list_hash[item] = []
-end
+# def create_list(db, list_hash, items, quantity)
+#   db.execute("INSERT INTO items (item_name, item_quantity) VALUES (?, ?)", [item_name, item_quantity])
+#   list_hash = {}
+#   list_hash[item] = []
+# end
 
 # driver code
 puts "Enter your name to access your list"
